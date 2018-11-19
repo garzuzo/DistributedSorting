b6f925ef42e822b5759ebc601efd2196ab30d123
garzuxo <garzuxo@gmail.com>
2018-11-19 15:21:43 -0500

    added composites 1-6 and mergesorts

---
+++ DSorting/Sorters/MergeSort3/src/main/java/co/edu/icesi/frascati/distributed/MergeSort.java
@@ -0,0 +1,77 @@
+package co.edu.icesi.frascati.distributed;
+
+public class MergeSort implements IMergeSort {
+
+	@Override
+	public String[] partialMergeSort(String[] arr, int l, int r) {
+
+		arr = sort(arr, l, r);
+		return arr;
+	}
+
+	public void merge(String arr[], int l, int m, int r) {
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		String L[] = new String[n1];
+		String R[] = new String[n2];
+
+		/* Copy data to temp arrays */
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		/* Merge the temp arrays */
+
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarry array
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i].compareTo(R[j]) <= 0) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	// Main function that sorts arr[l..r] using
+	// merge()
+	public String[] sort(String arr[], int l, int r) {
+		if (l < r) {
+			// Find the middle point
+			int m = (l + r) / 2;
+
+			// Sort first and second halves
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+
+			// Merge the sorted halves
+			merge(arr, l, m, r);
+		}
+		return arr;
+	}
+
+}
