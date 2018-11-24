23c595e191e1760c13b23a23064a5baea6b2f56d
mishale22 <sami_na11@hotmail.com>
2018-11-24 12:14:32 -0500

    Controller six nodes

---
+++ DSorting/CentralNode/SixNodes/Controller/src/main/java/co/edu/icesi/frascati/distributed/IMerge.java
@@ -0,0 +1,9 @@
+package co.edu.icesi.frascati.distributed;
+
+import org.osoa.sca.annotations.Service;
+
+@Service
+public interface IMerge {
+
+	public String[] merge(String[] arr1, String[] arr2);
+}
